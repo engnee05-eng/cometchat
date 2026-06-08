059ff75d8e4bc7ba9d81fcbda7fc16022a29c5f3
engnee05-eng <engnee05@gmail.com>
2026-06-08 22:31:18 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Check if running in GitHub Actions and resolve repo name
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const repoName = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : '';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: isGithubActions ? `/${repoName}/` : '/',
 })
+
